23664fe10808d908e44d97454ff72d4458e5fe3c
krzysztoffila <umowy.fila@gmail.com>
2024-07-07 18:04:34 +0200

    Add data

---
+++ script.py
@@ -0,0 +1,19 @@
+# create the initial variables below
+age = 28
+sex = 0
+bmi = 26.2 
+num_of_children = 3
+smoker = 0
+
+# Add insurance estimate formula below
+
+# Age Factor
+
+
+# BMI Factor
+
+
+# Male vs. Female Factor
+
+
+# Extra Practice
