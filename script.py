6c67c742eb93cef1373e100bf1f1e2622570ff41
krzysztoffila <umowy.fila@gmail.com>
2024-07-08 16:02:34 +0200

    Add new data

--- script.py
+++ script.py
@@ -1,17 +1,8 @@
-def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker, name):
-  estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
-  print("The estimated insurance cost for "+ str(name) +" is " + str(estimated_cost) + " dollars.")
+# Function to estimate insurance cost:
+def estimate_insurance_cost(name, age, sex, num_of_children, smoker):
+  estimated_cost = 400*age - 128*sex + 425*num_of_children + 10000*smoker - 2500
+  print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
   return estimated_cost
-
-# Estimate Maria's insurance cost
-maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0, name = "Maria")
-
-# Initial variables for Omar
-age = 35
-sex = 1 
-bmi = 22.2
-num_of_children = 0
-smoker = 1  
-
-# Estimate Omar's insurance cost 
-omar_insurance_cost = calculate_insurance_cost(35,1,22.2,0,1, name = "Omar")
+ 
+# Estimate Keanu's insurance cost
+keanu_insurance_cost = estimate_insurance_cost(name = 'Keanu', age = 29, sex = 1, num_of_children = 3, smoker = 1)
