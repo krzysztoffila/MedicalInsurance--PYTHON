2d13e47a0d19cdac40661da1fbd136e881abc7f0
krzysztoffila <umowy.fila@gmail.com>
2024-07-08 22:06:12 +0200

    Zip list, Inspection, Testing

--- script.py
+++ script.py
@@ -14,4 +14,12 @@ rohan_insurance_cost = estimate_insurance_cost(name =
 # Estimate Valentina's insurance cost
 valentina_insurance_cost = estimate_insurance_cost(name = "Valentina", age = 53, sex = 0, bmi = 31.4, num_of_children = 0, smoker = 1)
 
-# Add your code here
+names = ["Maria", "Rohan", "Valentina"]
+insurance_costs = [4150.0, 5320.0, 35210.0]
+insurance_data = list(zip(names, insurance_costs))
+print("Here is the actual insurance cost data: " + str(insurance_data))
+estimated_insurance_data = []
+estimated_insurance_data.append(("Maria", maria_insurance_cost))
+estimated_insurance_data.append(("Rohan", rohan_insurance_cost))
+estimated_insurance_data.append(("Valentina", valentina_insurance_cost))
+print("Here is the estimated insurance cost data: " + str(estimated_insurance_data))
