3ef216b4ffa09ce89f4380cc94ea4110ce494265
krzysztoffila <umowy.fila@gmail.com>
2024-07-08 07:29:18 +0200

    Add name parameter

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
-def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker):
+def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker, name):
   estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
-  print("The estimated insurance cost for this person is " + str(estimated_cost) + " dollars.")
+  print("The estimated insurance cost for "+ str(name) +" is " + str(estimated_cost) + " dollars.")
   return estimated_cost
 
 # Estimate Maria's insurance cost
-maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0)
+maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0, name = "Maria")
 
 # Initial variables for Omar
 age = 35
@@ -14,4 +14,4 @@ num_of_children = 0
 smoker = 1  
 
 # Estimate Omar's insurance cost 
-omar_insurance_cost = calculate_insurance_cost(35,1,22.2,0,1)
+omar_insurance_cost = calculate_insurance_cost(35,1,22.2,0,1, name = "Omar")
